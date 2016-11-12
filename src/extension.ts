4c8af3bdac37c8bb9ecc66fec9db7b1b242d543a
Cayde Dixon <Cayde Dixon>
2016-11-13 01:50:45 +1100

    Update to use the console in output

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,50 @@
-import {window, workspace, commands, ExtensionContext, QuickPickItem} from 'vscode'; 
+import {window, workspace, commands, ExtensionContext, QuickPickItem, OutputChannel, Disposable} from 'vscode'; 
 import * as proc from 'child_process'
 
 var cacheNeeded: boolean = true;
 
 var TaskCache: QuickPickItem[] = [];
+var outputChannel: OutputChannel = null;
 
 export function activate(context: ExtensionContext) {
 	// const cwd = workspace.rootPath;
-	
-
+	outputChannel = window.createOutputChannel("Gradle")
 	const disposable = commands.registerCommand('gradle', () => {
-		return window.showQuickPick(list()).then(task => {
-			proc.exec(
+		
+		return window.showQuickPick(list()).then((task: QuickPickItem) => {
+			var statusbar: Disposable = window.setStatusBarMessage("Running...")
+			outputChannel.show();
+			var process = proc.exec(
 				cmd() + " " + task.label, 
 				{cwd: workspace.rootPath}, 
 				(err, stdout, stderr) => {
 					if (err) window.showErrorMessage("An error occured");
 					else window.showInformationMessage("Success!");
-				})
+					outputChannel.append(stdout);
+				});
+			process.stdout.on("data", data => outputChannel.append(data.toString()));
+			process.stderr.on("data", data => outputChannel.append("[ERR] " + data));
+			statusbar.dispose(); 
 		})
 	});
 
 	context.subscriptions.push(disposable);
-}
+} 
 
 function cmd(): string { return workspace.getConfiguration().get("gradle.useCommand", "gradlew"); }
 
-function list(): Promise<QuickPickItem[]> {
+function list(): Thenable<QuickPickItem[]> {
 	const regex = /$\s*([a-z0-9]+)\s-\s(.*)$/mgi;
 	
 	return new Promise(resolve => {
-		if (cacheNeeded) { proc.exec(cmd() + " tasks", {cwd: workspace.rootPath}, (err, stdout, stderr) => {
+		if (cacheNeeded) {
+			outputChannel.show(); 
+			var process = proc.exec(cmd() + " tasks", {cwd: workspace.rootPath}, (err, stdout, stderr) => {
 				if (err) { return resolve([])}
 				var match: RegExpExecArray;
 				var items: QuickPickItem[] = [];
-				
-				while ((match = regex.exec(stdout.toString('utf8'))) !== null) {
+
+				while ((match = regex.exec(stdout.toString())) !== null) {
 					items.push({
 						label: match[1],
 						description: match[2], 
@@ -46,6 +55,9 @@ function list(): Promise<QuickPickItem[]> {
 				// cacheNeeded = false; //Maybe I'll implement this later...
 				return resolve(items);
 			});
+			process.stdout.on("data", data => outputChannel.append(data.toString()));
+			process.stderr.on("data", data => outputChannel.append("[ERR] " + data));
+			
 		}
 		else return resolve(TaskCache);
 	})
