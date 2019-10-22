a8ee9e31607fbcd3491cf2369463df3ed7a4664f
Richard Willis <willis.rh@gmail.com>
2019-10-22 22:37:23 +0200

    Add some basic tests

--- src/test/gradle/extension.test.ts
+++ src/test/gradle/extension.test.ts
@@ -1,19 +1,41 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 
+import TaskRegistry from '../../TaskRegistry';
+
 suite('With build.grade Extension Test Suite', () => {
   vscode.window.showInformationMessage('Started all tests');
 
-  test('should be present', () => {
+  test('it should be present', () => {
     assert.ok(vscode.extensions.getExtension('richardwillis.vscode-gradle'));
   });
 
-  test('should activate', () => {
+  test('it should activate', () => {
     const extension = vscode.extensions.getExtension(
       'richardwillis.vscode-gradle'
     );
     if (extension) {
       assert.equal(extension.isActive, true);
     }
+  });
+
+  test('it should load tasks', () => {
+    assert.equal(TaskRegistry.getTasks().length > 0, true);
+  });
+
+  test('it should successfully run a task', () => {
+    return vscode.commands.executeCommand('gradle:runtask', 'build');
+  });
+
+  test('it should fail when running a task that fails', () => {
+    return assert.rejects(async () =>
+      vscode.commands.executeCommand('gradle:runtask', 'INVALID_TASK_NAME')
+    );
+  });
+
+  test('it should refresh tasks', async () => {
+    TaskRegistry.clear();
+    await vscode.commands.executeCommand('gradle:refresh');
+    assert.equal(TaskRegistry.getTasks().length > 0, true);
   });
 });
