c55615695cbcfe0ae62b561c25e891086e757dc6
Richard Willis <willis.rh@gmail.com>
2020-02-17 20:35:52 +0100

    Test 3

--- bin/comment-pr.sh
+++ bin/comment-pr.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "RUN_ID: $RUN_ID"
+echo "RUN_NUMBER: $RUN_NUMBER"
+
+curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts
+
 curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts | jq '.artifacts[] | ("* [lib](" + .archive_download_url + ") (" + (.size_in_bytes * 0.000001|tostring)) + "MB)"'
 
 ARTIFACTS_LIST=$(curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts | jq '.artifacts[] | ("* [lib](" + .archive_download_url + ") (" + (.size_in_bytes * 0.000001|tostring)) + "MB)"')
@@ -7,5 +12,5 @@ ARTIFACTS_LIST=$(curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET
 echo "ARTIFACTS_LIST: ${ARTIFACTS_LIST}"
 
 curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
-  -X POST -d "{\"body\": \"Generated artifacts:${ARTIFACTS_LIST}\"}" \
+  -X POST -d "{\"body\": \"TEST Generated artifacts:${ARTIFACTS_LIST}\"}" \
   "https://api.github.com/repos/badsyntax/vscode-gradle/issues/${PR_NUMBER}/comments"
