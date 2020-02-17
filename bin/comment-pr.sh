b91a9cbed3383bedbf5d7bda786e0296d9efa3be
Richard Willis <willis.rh@gmail.com>
2020-02-17 20:49:12 +0100

    Test

--- bin/comment-pr.sh
+++ bin/comment-pr.sh
@@ -2,10 +2,8 @@
 
 echo "RUN_ID: $RUN_ID"
 echo "RUN_NUMBER: $RUN_NUMBER"
-
-curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts
-
-curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts | jq '.artifacts[] | ("* [lib](" + .archive_download_url + ") (" + (.size_in_bytes * 0.000001|tostring)) + "MB)"'
+echo "Pausing for 10 seconds..."
+sleep 10
 
 ARTIFACTS_LIST=$(curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts | jq '.artifacts[] | ("* [lib](" + .archive_download_url + ") (" + (.size_in_bytes * 0.000001|tostring)) + "MB)"')
 
