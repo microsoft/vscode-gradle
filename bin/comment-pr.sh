3400615eb331502e1233cd614024121b51aeecdc
Richard Willis <willis.rh@gmail.com>
2020-02-17 20:51:47 +0100

    Test

--- bin/comment-pr.sh
+++ bin/comment-pr.sh
@@ -2,10 +2,15 @@
 
 echo "RUN_ID: $RUN_ID"
 echo "RUN_NUMBER: $RUN_NUMBER"
-echo "Pausing for 10 seconds..."
-sleep 10
+echo "Pausing for 30 seconds..."
+sleep 30
 
-ARTIFACTS_LIST=$(curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts | jq '.artifacts[] | ("* [lib](" + .archive_download_url + ") (" + (.size_in_bytes * 0.000001|tostring)) + "MB)"')
+ARTIFACTS_LIST=$(\
+  curl -s \
+    -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+    -X GET https://api.github.com/repos/badsyntax/vscode-gradle/actions/runs/${RUN_ID}/artifacts \
+    | jq '.artifacts[] | ("* [lib](" + .archive_download_url + ") (" + (.size_in_bytes * 0.000001|tostring)) + "MB)"' \
+)
 
 echo "ARTIFACTS_LIST: ${ARTIFACTS_LIST}"
 
