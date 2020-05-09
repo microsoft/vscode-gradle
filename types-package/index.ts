4986dddc273eb03aabc7cc06650095502c38bb3c
Richard Willis <willis.rh@gmail.com>
2020-05-09 06:51:59 +0100

    Add onTasksLoaded

--- types-package/index.ts
+++ types-package/index.ts
@@ -1,8 +1,10 @@
-import { Output } from './lib/proto/gradle_tasks_pb';
-import { RunTaskHandler } from './lib/runTask';
+import * as vscode from 'vscode';
+import { Output, RunTaskRequest } from './lib/proto/gradle_tasks_pb';
+import { RunTaskHandler, RunTaskOpts } from './lib/runTask';
 
-export { Output, RunTaskHandler };
+export { Output, RunTaskRequest, RunTaskHandler, RunTaskOpts };
 
 export interface ExtensionApi {
   runTask: RunTaskHandler;
+  onTasksLoaded: vscode.Event<null>;
 }
