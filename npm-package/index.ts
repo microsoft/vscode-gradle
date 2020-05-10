24d4bbd655b7fa6325c88e70d8c89dcaf92cbafa
Richard Willis <willis.rh@gmail.com>
2020-05-10 20:48:49 +0100

    Export CancelTaskOpts

--- npm-package/index.ts
+++ npm-package/index.ts
@@ -4,12 +4,13 @@ import {
   CancelRunTaskRequest,
 } from './lib/proto/gradle_tasks_pb';
 import { OutputBuffer } from './lib/OutputBuffer';
-import type { Api, RunTaskOpts } from './lib/api';
+import type { Api, RunTaskOpts, CancelTaskOpts } from './lib/api';
 
 export {
   Output,
   RunTaskRequest,
   RunTaskOpts,
+  CancelTaskOpts,
   CancelRunTaskRequest,
   OutputBuffer,
   Api as ExtensionApi,
