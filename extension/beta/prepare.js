3626a8caac0aed5710d63993e613e38241018bc2
Richard Willis <willis.rh@gmail.com>
2020-05-24 08:33:35 +0100

    Linting fixes

--- extension/beta/prepare.js
+++ extension/beta/prepare.js
@@ -5,7 +5,11 @@ const betaPackageJson = require('./package-beta.json');
 
 Object.assign(packageJson, betaPackageJson);
 
-fs.writeFileSync('../package.json', JSON.stringify(packageJson, null, 2), 'utf8');
+fs.writeFileSync(
+  '../package.json',
+  JSON.stringify(packageJson, null, 2),
+  'utf8'
+);
 
 const readme = fs.readFileSync('./README.md', 'utf8');
 fs.writeFileSync('../README.md', readme, 'utf8');
