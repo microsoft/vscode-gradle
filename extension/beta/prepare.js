b01a1d1b8fe3f52fec1a225baf20cce9bcc1fe4f
Richard Willis <willis.rh@gmail.com>
2020-05-24 08:29:45 +0100

    Add beta files

---
+++ extension/beta/prepare.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const packageJson = require('../package.json');
+const betaPackageJson = require('./package-beta.json');
+
+Object.assign(packageJson, betaPackageJson);
+
+fs.writeFileSync('../package.json', JSON.stringify(packageJson, null, 2), 'utf8');
+
+const readme = fs.readFileSync('./README.md', 'utf8');
+fs.writeFileSync('../README.md', readme, 'utf8');
