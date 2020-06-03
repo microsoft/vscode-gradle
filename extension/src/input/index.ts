5ed09c30c084e0c46dbdd4c8bb707d86e6107e7a
Richard Willis <willis.rh@gmail.com>
2020-06-03 08:16:35 +0100

    Trim task args input

--- extension/src/input/index.ts
+++ extension/src/input/index.ts
@@ -2,8 +2,14 @@ import * as vscode from 'vscode';
 import { TaskArgs } from '../stores/types';
 
 export function getTaskArgs(): Thenable<TaskArgs | undefined> {
-  return vscode.window.showInputBox({
-    placeHolder: 'For example: --info',
-    ignoreFocusOut: true,
-  });
+  return vscode.window
+    .showInputBox({
+      placeHolder: 'For example: --info',
+      ignoreFocusOut: true,
+    })
+    .then((value: string | undefined) => {
+      if (value !== undefined) {
+        return value.trim();
+      }
+    });
 }
