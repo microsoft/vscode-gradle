4f4e5a4517bc93d6bfa30f46cf15817a89763889
Richard Willis <willis.rh@gmail.com>
2020-10-01 21:03:54 +0100

    Simplify debounce decorator

--- extension/src/decorators/decorators.ts
+++ extension/src/decorators/decorators.ts
@@ -29,34 +29,13 @@ export interface DebounceReducer<T> {
   (previousValue: T, ...args: any[]): T;
 }
 
-export function debounce<T>(
-  delay: number,
-  reducer?: DebounceReducer<T>,
-  initialValueProvider?: () => T
-): Function {
+export function debounce<T>(delay: number): Function {
   return createDecorator((fn, key) => {
     const timerKey = `$debounce$${key}`;
-    const resultKey = `$debounce$result$${key}`;
-
     return function (this: any, ...args: any[]): void {
-      if (!this[resultKey]) {
-        this[resultKey] = initialValueProvider
-          ? initialValueProvider()
-          : undefined;
-      }
-
       clearTimeout(this[timerKey]);
-
-      if (reducer) {
-        this[resultKey] = reducer(this[resultKey], ...args);
-        args = [this[resultKey]];
-      }
-
       this[timerKey] = setTimeout(() => {
         fn.apply(this, args);
-        this[resultKey] = initialValueProvider
-          ? initialValueProvider()
-          : undefined;
       }, delay);
     };
   });
