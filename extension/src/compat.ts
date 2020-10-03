a04a05bf12c38acf538e3daa13fd86cffca92550
Richard Willis <willis.rh@gmail.com>
2020-10-03 16:04:41 +0100

    Add runBuild to extension API (#706)
    * Fix compat checks
    
    * Expose runBuild public API

--- extension/src/compat.ts
+++ extension/src/compat.ts
@@ -2,21 +2,19 @@ import * as vscode from 'vscode';
 
 export const JAVA_LANGUAGE_EXTENSION_ID = 'redhat.java';
 export const JAVA_DEBUGGER_EXTENSION_ID = 'vscjava.vscode-java-debug';
-export const JAVA_CONFIGURATION_UPDATE_COMMAND =
-  'java.projectConfiguration.update';
 
 export function isJavaLanguageSupportExtensionActivated(): boolean {
   const javaExt:
     | vscode.Extension<unknown>
-    | undefined = getJavaDebuggerExtension();
-  return !!javaExt && javaExt.isActive;
+    | undefined = getJavaLanguageSupportExtension();
+  return javaExt?.isActive || false;
 }
 
 export function isJavaDebuggerExtensionActivated(): boolean {
   const javaExt:
     | vscode.Extension<unknown>
-    | undefined = getJavaLanguageSupportExtension();
-  return !!javaExt && javaExt.isActive;
+    | undefined = getJavaDebuggerExtension();
+  return javaExt?.isActive || false;
 }
 
 export function getJavaLanguageSupportExtension():
