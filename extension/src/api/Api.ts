9b1c516e96b24d874d86648f0003c34b04759ebc
Richard Willis <willis.rh@gmail.com>
2020-10-14 22:18:31 +0100

    Simplify API
    Waiting for the tasks to be loaded before running a task is unnecessary

--- extension/src/api/Api.ts
+++ extension/src/api/Api.ts
@@ -2,7 +2,6 @@ import * as vscode from 'vscode';
 import { Output } from '../proto/gradle_pb';
 import { Logger, logger } from '../logger';
 import { GradleTasksTreeDataProvider } from '../views';
-import { GradleTaskDefinition } from '../tasks';
 import { GradleClient } from '../client';
 import { Icons } from '../icons';
 import { getRunBuildCancellationKey } from '../client/CancellationKeys';
@@ -49,9 +48,8 @@ export class Api {
     return this.client.onDidConnect(callback);
   }
 
-  public async runTask(opts: RunTaskOpts): Promise<void> {
+  public runTask(opts: RunTaskOpts): Promise<void> {
     const taskArgs = (opts.args || []).filter(Boolean);
-    await this.findTask(opts.projectFolder, opts.taskName);
     const runBuildArgs = [opts.taskName].concat(taskArgs);
     const runBuildOpts = {
       ...opts,
@@ -60,7 +58,7 @@ export class Api {
     return this.runBuild(runBuildOpts);
   }
 
-  public async runBuild(opts: RunBuildOpts): Promise<void> {
+  public runBuild(opts: RunBuildOpts): Promise<void> {
     return this.client.runBuild(
       opts.projectFolder,
       opts.cancellationKey ||
@@ -74,7 +72,7 @@ export class Api {
     );
   }
 
-  public async cancelRunTask(opts: CancelTaskOpts): Promise<void> {
+  public cancelRunTask(opts: CancelTaskOpts): Promise<void> {
     const args = opts.taskName ? [opts.taskName] : [];
     const cancelBuildOpts = {
       projectFolder: opts.projectFolder,
@@ -82,6 +80,11 @@ export class Api {
       cancellationKey: opts.cancellationKey,
     };
     return this.cancelRunBuild(cancelBuildOpts);
+  }
+
+  public cancelRunBuild(opts: CancelBuildOpts): Promise<void> {
+    const cancellationKey = this.getRunBuildCancellationKey(opts);
+    return this.client.cancelBuild(cancellationKey);
   }
 
   public cancelAllBuilds(): Promise<void> {
@@ -98,31 +101,6 @@ export class Api {
       );
     }
     return getRunBuildCancellationKey(opts.projectFolder, opts.args);
-  }
-
-  public async cancelRunBuild(opts: CancelBuildOpts): Promise<void> {
-    const cancellationKey = this.getRunBuildCancellationKey(opts);
-    return this.client.cancelBuild(cancellationKey);
-  }
-
-  private async findTask(
-    projectFolder: string,
-    taskName: string
-  ): Promise<vscode.Task> {
-    const tasks = await vscode.tasks.fetchTasks({ type: 'gradle' });
-    if (!tasks) {
-      throw new Error('Unable to load gradle tasks');
-    }
-    const foundTask = tasks.find((task) => {
-      const definition = task.definition as GradleTaskDefinition;
-      return (
-        task.name === taskName && definition.projectFolder === projectFolder
-      );
-    });
-    if (!foundTask) {
-      throw new Error(`Unable to find task: ${taskName}`);
-    }
-    return foundTask;
   }
 
   public getTasksTreeProvider(): GradleTasksTreeDataProvider {
