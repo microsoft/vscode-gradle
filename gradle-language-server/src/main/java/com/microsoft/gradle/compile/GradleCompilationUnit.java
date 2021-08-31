063f57cb04ffaf85ef4d23ea54a708dccbc50434
Shi Chen <chenshi@microsoft.com>
2021-08-31 23:51:25 +0800

    feat: Support compiling and pushing diagnostics (#962)

---
+++ gradle-language-server/src/main/java/com/microsoft/gradle/compile/GradleCompilationUnit.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.microsoft.gradle.compile;
+
+import java.security.CodeSource;
+
+import org.codehaus.groovy.control.CompilationUnit;
+import org.codehaus.groovy.control.CompilerConfiguration;
+
+import groovy.lang.GroovyClassLoader;
+
+public class GradleCompilationUnit extends CompilationUnit {
+  private Integer version;
+
+  public GradleCompilationUnit(CompilerConfiguration configuration, CodeSource codeSource, GroovyClassLoader loader, Integer version) {
+    super(configuration, codeSource, loader);
+    this.version = version;
+  }
+
+  public Integer getVersion() {
+    return this.version;
+  }
+}
---
+++ gradle-language-server/src/main/java/com/microsoft/gradle/utils/LSPUtils.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.microsoft.gradle.utils;
+
+import org.codehaus.groovy.syntax.SyntaxException;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+
+public class LSPUtils {
+  public static Range toRange(SyntaxException exp) {
+    // LSP Range start from 0, while groovy classes start from 1
+    return new Range(new Position(exp.getStartLine() - 1, exp.getStartColumn() - 1),
+        new Position(exp.getEndLine() - 1, exp.getEndColumn() - 1));
+  }
+}
