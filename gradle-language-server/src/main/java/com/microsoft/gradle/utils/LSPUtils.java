a4f42e6338ddc76fd9795e00db75356ee9471fb8
Shi Chen <chenshi@microsoft.com>
2021-09-06 10:36:05 +0800

    feat: Support Basic Auto Completion (#971)

--- gradle-language-server/src/main/java/com/microsoft/gradle/utils/LSPUtils.java
+++ gradle-language-server/src/main/java/com/microsoft/gradle/utils/LSPUtils.java
@@ -11,6 +11,7 @@
 package com.microsoft.gradle.utils;
 
 import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.ast.stmt.Statement;
 import org.codehaus.groovy.syntax.SyntaxException;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
@@ -26,6 +27,12 @@ public class LSPUtils {
     // LSP Range start from 0, while groovy expressions start from 1
     return new Range(new Position(expression.getLineNumber() - 1, expression.getColumnNumber() - 1),
         new Position(expression.getLastLineNumber() - 1, expression.getLastColumnNumber() - 1));
+  }
+
+  public static Range toRange(Statement statement) {
+    // LSP Range start from 0, while groovy expressions start from 1
+    return new Range(new Position(statement.getLineNumber() - 1, statement.getColumnNumber() - 1),
+        new Position(statement.getLastLineNumber() - 1, statement.getLastColumnNumber() - 1));
   }
 
   public static Range toDependencyRange(Expression expression) {
