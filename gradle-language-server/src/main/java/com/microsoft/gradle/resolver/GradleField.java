15b95feb246badea0993402d8fcc1814fcc3c4e1
Shi Chen <chenshi@microsoft.com>
2021-11-29 09:58:49 +0800

    chore: Add tests for language server (#1092)

--- gradle-language-server/src/main/java/com/microsoft/gradle/resolver/GradleField.java
+++ gradle-language-server/src/main/java/com/microsoft/gradle/resolver/GradleField.java
@@ -6,4 +6,9 @@ package com.microsoft.gradle.resolver;
 public class GradleField {
   public String name;
   public boolean deprecated;
+
+  public GradleField(String name, boolean deprecated) {
+    this.name = name;
+    this.deprecated = deprecated;
+  }
 }
