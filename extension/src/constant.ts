a955b2b807b0bfd12a639cb4a927b15fc6caf485
Shi Chen <chenshi@microsoft.com>
2021-11-29 16:21:01 +0800

    better command palette

---
+++ extension/src/constant.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Context {
+  export const ACTIVATION_CONTEXT_KEY = 'gradle:extensionActivated';
+}
