473f1fac40ef83561eb9f569e827469f103dc294
Shi Chen <chenshi@microsoft.com>
2022-07-06 15:26:35 +0800

    chore: Report Gradle version of root project via telemetry (#1243)

--- extension/src/rootProject/RootProject.ts
+++ extension/src/rootProject/RootProject.ts
@@ -1,12 +1,22 @@
 import * as vscode from "vscode";
+import { sendInfo } from "vscode-extension-telemetry-wrapper";
 import { Environment } from "../proto/gradle_pb";
+import { isTest } from "../util";
 
 export class RootProject {
     private environment?: Environment;
+    private gradleVersion?: string;
     constructor(private readonly workspaceFolder: vscode.WorkspaceFolder, private readonly projectUri: vscode.Uri) {}
 
     public setEnvironment(environment: Environment): void {
         this.environment = environment;
+        const gradleVersion = environment.getGradleEnvironment()?.getGradleVersion();
+        if (gradleVersion && gradleVersion !== this.gradleVersion) {
+            this.gradleVersion = gradleVersion;
+            if (!isTest()) {
+                sendInfo("", { name: "changeGradleVersion", gradleVersion: gradleVersion });
+            }
+        }
     }
 
     public getEnvironment(): Environment | undefined {
