01900b19ba0bb49aedb38c8f26faaac1784fa1c5
Shi Chen <chenshi@microsoft.com>
2022-07-06 15:26:35 +0800

    chore: Report Gradle version of root project via telemetry (#1243)

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { initialize, instrumentOperation } from "vscode-extension-telemetry-wrapper";
+import { initializeFromJsonFile, instrumentOperation } from "vscode-extension-telemetry-wrapper";
 
 import { Api } from "./api";
 import { Extension } from "./Extension";
@@ -7,23 +7,13 @@ import { Extension } from "./Extension";
 let extension: Extension;
 
 export async function activate(context: vscode.ExtensionContext): Promise<Api> {
-    initializeTelemetry();
+    await initializeFromJsonFile(context.asAbsolutePath("./package.json"), { firstParty: true });
     return instrumentOperation("activation", activateExtension)(context);
 }
 
 function activateExtension(_operationId: string, context: vscode.ExtensionContext): Api {
     extension = new Extension(context);
     return extension.getApi();
-}
-
-function initializeTelemetry(): void {
-    const ext = vscode.extensions.getExtension("vscjava.vscode-gradle");
-    const packageInfo = ext ? ext.packageJSON : undefined;
-    if (packageInfo && packageInfo.aiKey) {
-        initialize(packageInfo.id, packageInfo.version, packageInfo.aiKey, {
-            firstParty: true,
-        });
-    }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
