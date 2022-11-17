04e8483832fe6fa7fd682c09cbe6c3a975a03cdd
Shi Chen <chenshi@microsoft.com>
2022-11-17 12:56:55 +0800

    fix: Duplicate temp plugin jars have been created (#1297)

--- gradle-server/src/main/java/com/github/badsyntax/gradle/utils/PluginUtils.java
+++ gradle-server/src/main/java/com/github/badsyntax/gradle/utils/PluginUtils.java
@@ -3,36 +3,85 @@
 
 package com.github.badsyntax.gradle.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class PluginUtils {
-	public static File createInitScript() throws IOException {
-		File initScript = File.createTempFile("init-build", ".gradle");
-		initScript.deleteOnExit();
-		File pluginFile = File.createTempFile("custom-plugin", ".jar");
-		pluginFile.deleteOnExit();
-		createPluginJar("/gradle-plugin.jar", pluginFile);
-		createTemplateScript(pluginFile, initScript);
-		return initScript;
+
+	private static final String MESSAGE_DIGEST_ALGORITHM = "SHA-256";
+	private static final String PLUGIN_JAR_PATH = "/gradle-plugin.jar";
+
+	public static File getInitScript() {
+		try {
+			// handle plugin jar
+			InputStream input = PluginUtils.class.getResourceAsStream(PLUGIN_JAR_PATH);
+			byte[] pluginJarBytes = readFully(input);
+			byte[] pluginJarDigest = getContentDigest(pluginJarBytes);
+			String pluginJarName = bytesToHex(pluginJarDigest) + ".jar";
+			File pluginJarFile = new File(System.getProperty("java.io.tmpdir"), pluginJarName);
+			if (needReplaceContent(pluginJarFile, pluginJarDigest)) {
+				Files.write(pluginJarFile.toPath(), pluginJarBytes);
+			}
+			// handle init script
+			String pluginJarUnixPath = pluginJarFile.getAbsolutePath().replace("\\", "/");
+			String initScriptContent = "initscript {\n" + "	dependencies {\n" + "		classpath files('"
+					+ pluginJarUnixPath + "')\n" + "	}\n" + "}\n" + "\n" + "allprojects {\n"
+					+ "	apply plugin: com.microsoft.gradle.GradlePlugin\n" + "}\n";
+			byte[] initScriptBytes = initScriptContent.getBytes();
+			byte[] initScriptDigest = getContentDigest(initScriptBytes);
+			String initScriptName = bytesToHex(initScriptDigest) + ".gradle";
+			File initScriptFile = new File(System.getProperty("java.io.tmpdir"), initScriptName);
+			if (needReplaceContent(initScriptFile, initScriptDigest)) {
+				Files.write(initScriptFile.toPath(), initScriptBytes);
+			}
+			return initScriptFile;
+		} catch (IOException | NoSuchAlgorithmException e) {
+			return null;
+		}
 	}
 
-	private static void createPluginJar(String resource, File outputFile) throws IOException {
-		InputStream input = PluginUtils.class.getResourceAsStream(resource);
-		Files.copy(input, outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		input.close();
+	// InputStream.readAllBytes is not available in Java 8, so we have this method
+	// to convert InputStream to byte array
+	public static byte[] readFully(InputStream input) throws IOException {
+		byte[] buffer = new byte[8192];
+		int bytesRead;
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
+		return output.toByteArray();
 	}
 
-	private static void createTemplateScript(File pluginFile, File outputFile) throws IOException {
-		String pluginFilePath = pluginFile.getAbsolutePath().replace("\\", "/");
-		// @formatter:off
-		String template = "initscript {\n" + "    dependencies {\n" + "        classpath files('" + pluginFilePath
-				+ "')\n" + "    }\n" + "}\n" + "\n" + "allprojects {\n"
-				+ "    apply plugin: com.microsoft.gradle.GradlePlugin\n" + "}\n";
-		// @formatter:on
-		Files.write(outputFile.toPath(), template.getBytes());
+	private static boolean needReplaceContent(File initScript, byte[] checksum)
+			throws IOException, NoSuchAlgorithmException {
+		if (!initScript.exists() || initScript.length() == 0) {
+			return true;
+		}
+
+		byte[] digest = getContentDigest(Files.readAllBytes(initScript.toPath()));
+		if (Arrays.equals(digest, checksum)) {
+			return false;
+		}
+		return true;
+	}
+
+	private static byte[] getContentDigest(byte[] contentBytes) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance(MESSAGE_DIGEST_ALGORITHM);
+		md.update(contentBytes);
+		return md.digest();
+	}
+
+	private static String bytesToHex(byte[] in) {
+		final StringBuilder builder = new StringBuilder();
+		for (byte b : in) {
+			builder.append(String.format("%02x", b));
+		}
+		return builder.toString();
 	}
 }
