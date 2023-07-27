6baca615e68e3e1ea3476c1c61105292a6713aa2
Sheng Chen <sheche@microsoft.com>
2023-07-27 09:37:42 +0800

    Handle the project description file for build targets (#1355)
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- extension/jdtls.ext/com.microsoft.gradle.bs.importer/src/com/microsoft/gradle/bs/importer/Utils.java
+++ extension/jdtls.ext/com.microsoft.gradle.bs.importer/src/com/microsoft/gradle/bs/importer/Utils.java
@@ -1,13 +1,41 @@
 package com.microsoft.gradle.bs.importer;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jdt.ls.core.internal.ProjectUtils;
+
+import ch.epfl.scala.bsp4j.BuildTarget;
 
 public class Utils {
     private Utils() {}
 
     public static boolean isGradleBuildServerProject(IProject project) {
         return ProjectUtils.hasNature(project, GradleBuildServerProjectNature.NATURE_ID);
+    }
+
+    /**
+     * Map the build targets by their paths, the paths are get from the uri.
+     * @param buildTargets
+     * @return
+     */
+    public static Map<URI, List<BuildTarget>> mapBuildTargetsByProjectPath(List<BuildTarget> buildTargets) {
+        return buildTargets.stream().collect(Collectors.groupingBy(target -> {
+            return getUriWithoutQuery(target.getId().getUri());
+        }));
+    }
+
+    public static URI getUriWithoutQuery(String uriString) {
+        try {
+            URI uri = new URI(uriString);
+            return new URI(uri.getScheme(), uri.getHost(), uri.getPath(), null, uri.getFragment());
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("Invalid uri: " + uriString, e);
+        }
     }
 
 }
