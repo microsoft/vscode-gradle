f4a22316e1bcfdd0c133bc05f2d41385db634343
Sheng Chen <sheche@microsoft.com>
2023-07-31 07:41:08 +0800

    Pass preference to build server (#1359)

---
+++ extension/jdtls.ext/com.microsoft.gradle.bs.importer/src/com/microsoft/gradle/bs/importer/model/BuildServerPreferences.java
@@ -0,0 +1,69 @@
+package com.microsoft.gradle.bs.importer.model;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The data used in 'build/initialize' request.
+ */
+public class BuildServerPreferences {
+    private String gradleJavaHome;
+    private String gradleVersion;
+    private String gradleHome;
+    private String gradleUserHome;
+    private List<String> gradleArguments;
+    private List<String> gradleJvmArguments;
+
+    public BuildServerPreferences() {
+        gradleArguments = Collections.emptyList();
+        gradleJvmArguments = Collections.emptyList();
+    }
+
+    public String getGradleJavaHome() {
+        return gradleJavaHome;
+    }
+
+    public void setGradleJavaHome(String gradleJavaHome) {
+        this.gradleJavaHome = gradleJavaHome;
+    }
+
+    public String getGradleVersion() {
+        return gradleVersion;
+    }
+
+    public void setGradleVersion(String gradleVersion) {
+        this.gradleVersion = gradleVersion;
+    }
+
+    public String getGradleHome() {
+        return gradleHome;
+    }
+
+    public void setGradleHome(String gradleHome) {
+        this.gradleHome = gradleHome;
+    }
+
+    public String getGradleUserHome() {
+        return gradleUserHome;
+    }
+
+    public void setGradleUserHome(String gradleUserHome) {
+        this.gradleUserHome = gradleUserHome;
+    }
+
+    public List<String> getGradleArguments() {
+        return gradleArguments;
+    }
+
+    public void setGradleArguments(List<String> gradleArguments) {
+        this.gradleArguments = gradleArguments;
+    }
+
+    public List<String> getGradleJvmArguments() {
+        return gradleJvmArguments;
+    }
+
+    public void setGradleJvmArguments(List<String> gradleJvmArguments) {
+        this.gradleJvmArguments = gradleJvmArguments;
+    }
+}
