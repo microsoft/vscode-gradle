83a3054c7ad2f61d2e00b75f9eb32bef5117e208
Jinbo Wang <jinbwan@microsoft.com>
2023-08-17 17:00:47 +0800

    Add license header for the new builder

--- extension/jdtls.ext/com.microsoft.gradle.bs.importer/src/com/microsoft/java/builder/JavaNameEnvironment.java
+++ extension/jdtls.ext/com.microsoft.gradle.bs.importer/src/com/microsoft/java/builder/JavaNameEnvironment.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
 package com.microsoft.java.builder;
 
 import org.eclipse.core.resources.IWorkspaceRoot;
