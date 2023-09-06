4afa1bede65732de655f26fdea000f274845017e
Sheng Chen <sheche@microsoft.com>
2023-09-06 09:26:11 +0800

    Enable Gradle build server by default (#1404)
    - Set the default value of 'java.gradle.buildServer.enabled' to 'on'.
    - Hint users to reload if the 'java.gradle.buildServer.enabled' changes.
    - Remove Buildship configuration from the project description if the project
      already has the build server nature.
    ---------
    
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- extension/src/bs/BuildServerController.ts
+++ extension/src/bs/BuildServerController.ts
@@ -1,7 +1,21 @@
-import { Disposable, ExtensionContext, OutputChannel, commands, languages, window } from "vscode";
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import {
+    ConfigurationChangeEvent,
+    Disposable,
+    ExtensionContext,
+    OutputChannel,
+    commands,
+    languages,
+    window,
+    workspace,
+} from "vscode";
 import { GradleBuildLinkProvider } from "./GradleBuildLinkProvider";
 import { sendInfo } from "vscode-extension-telemetry-wrapper";
 import { OpenBuildOutputValue, getOpenBuildOutput } from "../util/config";
+import * as path from "path";
+import * as fse from "fs-extra";
 
 const APPEND_BUILD_LOG_CMD = "_java.gradle.buildServer.appendBuildLog";
 const LOG_CMD = "_java.gradle.buildServer.log";
@@ -48,6 +62,29 @@ export class BuildServerController implements Disposable {
             commands.registerCommand(SEND_TELEMETRY_CMD, (jsonString: string) => {
                 const log = JSON.parse(jsonString);
                 sendInfo("", log);
+            }),
+            workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
+                if (e.affectsConfiguration("java.gradle.buildServer.enabled")) {
+                    const storagePath = context.storageUri?.fsPath;
+                    if (!storagePath) {
+                        return;
+                    }
+
+                    const msg =
+                        "Please reload to make the change of 'java.gradle.buildServer.enabled' take effect. Reload now?";
+                    const action = "Reload";
+                    window.showWarningMessage(msg, action).then(async (selection) => {
+                        if (action === selection) {
+                            // generate a flag file to make it a clean reload.
+                            // https://github.com/redhat-developer/vscode-java/blob/d02cf8ecfee1f3f528770a51ada825d522356967/src/settings.ts#L46
+                            const jlsWorkspacePath = path.resolve(storagePath, "..", "redhat.java", "jdt_ws");
+                            await fse.ensureDir(jlsWorkspacePath);
+                            const flagFile = path.resolve(jlsWorkspacePath, ".cleanWorkspace");
+                            await fse.writeFile(flagFile, "");
+                            commands.executeCommand("workbench.action.reloadWindow");
+                        }
+                    });
+                }
             })
         );
     }
