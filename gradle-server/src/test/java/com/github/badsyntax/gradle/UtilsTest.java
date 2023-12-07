18354a00f767e90a5f885f99657511a576b69673
Sheng Chen <sheche@microsoft.com>
2023-12-06 20:46:19 -0800

    fix: Enable the gradle task server tests (#1459)
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- gradle-server/src/test/java/com/github/badsyntax/gradle/UtilsTest.java
+++ gradle-server/src/test/java/com/github/badsyntax/gradle/UtilsTest.java
@@ -3,75 +3,43 @@
 
 package com.github.badsyntax.gradle;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import com.github.badsyntax.gradle.utils.Utils;
 import io.github.g00fy2.versioncompare.Version;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class UtilsTest {
 
-	@Test
-	public void testJava21Compatibility() {
-		Version javaVersion = new Version("21");
-		Version gradleVersion = new Version("8.4");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("8.1");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
+	private final String gradleVersion;
+	private final String javaVersion;
+	private final boolean expectedResult;
+
+	public UtilsTest(String gradleVersion, String javaVersion, boolean expectedResult) {
+		this.gradleVersion = gradleVersion;
+		this.javaVersion = javaVersion;
+		this.expectedResult = expectedResult;
+	}
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][]{{"8.5", "21", false}, {"8.1", "21", true}, {"8.1", "20.0.2", false},
+				{"8.0.1", "20.0.2", true}, {"7.6.0", "19.0.1", false}, {"7.5.0", "19.0.1", true},
+				{"7.3.0-rc2", "17.0.1", false}, {"7.2.0", "17.0.1", true}, {"7.2", "16.0.1", false},
+				{"6.8.3", "16.0.1", true}, {"7.2", "11.0.11.9-snapshot", false}, {"4.7", "11.0.11.9-snapshot", true},
+				{"4.3", "1.8.0_301", false}, {"1.8", "1.8.0_301", true}});
 	}
 
 	@Test
-	public void testJava20Compatibility() {
-		Version javaVersion = new Version("20.0.2");
-		Version gradleVersion = new Version("8.1");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("8.0.1");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-	}
-
-	@Test
-	public void testJava19Compatibility() {
-		Version javaVersion = new Version("19.0.1");
-		Version gradleVersion = new Version("7.6.0");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("7.5.0");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-	}
-
-	@Test
-	public void testJava17Compatibility() {
-		Version javaVersion = new Version("17.0.1");
-		Version gradleVersion = new Version("7.3.0-rc2");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("7.2.0");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-	}
-
-	@Test
-	public void testJava16Compatibility() {
-		Version javaVersion = new Version("16.0.1");
-		Version gradleVersion = new Version("7.2");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("6.8.3");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-	}
-
-	@Test
-	public void testJava11Compatibility() {
-		Version javaVersion = new Version("11.0.11.9-snapshot");
-		Version gradleVersion = new Version("7.2");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("4.7");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-	}
-
-	@Test
-	public void testJava8Compatibility() {
-		Version javaVersion = new Version("1.8.0_301");
-		Version gradleVersion = new Version("4.3");
-		assertFalse(Utils.hasCompatibilityError(gradleVersion, javaVersion));
-		gradleVersion = new Version("1.8");
-		assertTrue(Utils.hasCompatibilityError(gradleVersion, javaVersion));
+	public void testCompatibility() {
+		Version gradleVer = new Version(gradleVersion);
+		Version javaVer = new Version(javaVersion);
+		assertEquals(expectedResult, Utils.hasCompatibilityError(gradleVer, javaVer));
 	}
 }
