d47d78ed43e1d8aafbcec5a417323a6b9528b97d
wenyutang-ms <wenyutang@microsoft.com>
2025-08-13 10:43:10 +0800

    fix: update code

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -2,13 +2,13 @@ import * as vscode from "vscode";
 import { initializeFromJsonFile, instrumentOperation } from "vscode-extension-telemetry-wrapper";
 import { Api } from "./api";
 import { Extension } from "./Extension";
-import { TelemetryFilter } from "../util/telemetryFilter";
+import { TelemetryFilter } from "./util/telemetryFilter";
 
 let extension: Extension;
 
 export async function activate(context: vscode.ExtensionContext): Promise<Api> {
     await initializeFromJsonFile(context.asAbsolutePath("./package.json"), {
-        replacementOptions: [TelemetryFilter]
+        replacementOptions: [TelemetryFilter.hideUrlOption],
     });
     return instrumentOperation("activation", activateExtension)(context);
 }
--- extension/src/util/telemetryFilter.ts
+++ extension/src/util/telemetryFilter.ts
@@ -1,8 +1,8 @@
 import { ReplacementOption } from "@vscode/extension-telemetry";
 
 export class TelemetryFilter {
-    private static hideUrlOption: ReplacementOption = {
-        lookup: /https?:\/\/[^:\s]+:[^@\s]+@[^\s]+/g,  // match URLs with embedded credentials
-        replacementString: "<REDACTED: sensitive-url>"
+    public static hideUrlOption: ReplacementOption = {
+        lookup: /https?:\/\/[^:\s]+:[^@\s]+@[^\s]+/g, // match URLs with embedded credentials
+        replacementString: "<REDACTED: sensitive-url>",
     };
 }
