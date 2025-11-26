bdc84d1478b7face7e48ca81c3135d6ceeb9afcc
Changyong Gong <chagon@microsoft.com>
2025-11-26 13:32:24 +0800

    Revert "test: add some log"
    This reverts commit f00c711e557f6bb504deffe7f75ca78353f527c3.

--- extension/src/test/integration/gradle/extension.test.ts
+++ extension/src/test/integration/gradle/extension.test.ts
@@ -61,11 +61,6 @@ describe(getSuiteName("Extension"), () => {
                 }
                 await sleep(5 * 1000);
             }
-            console.log(
-                "Loaded tasks:",
-                tasks.map((t) => t.name)
-            );
-
             assert.ok(tasks);
             assert.strictEqual(tasks.length > 0, true);
             const helloTask = tasks.find(({ name }) => name === "hello");
